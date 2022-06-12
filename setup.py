14508f7b676372ced5a9b4154c565498186c8bdd
ekutner <>
2022-06-12 18:04:18 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'home-connect-async',
     packages = ['home_connect_async'],
-    version = '0.7.0-b4',
+    version = '0.7.0-b5',
     license='MIT',
     description = 'Async SDK for BSH Home Connect API',
     author = 'Eran Kutner',
