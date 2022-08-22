36d04b14182127fbd2fa242f91673cdf83e6208d
Yuval Aboulafia <yuval.abou@gmail.com>
2022-08-22 18:07:15 +0300

    Format callback_registry.py

--- home_connect_async/callback_registery.py
+++ home_connect_async/callback_registery.py
@@ -1,32 +1,39 @@
+"""Handle callbacks."""
 from __future__ import annotations
+
 import fnmatch
 import inspect
 import logging
 import re
-from typing import Callable
 from collections.abc import Sequence
+from typing import Callable, Any
 
-from .const import Events
 from .appliance import Appliance
+from .const import Events
 
 _LOGGER = logging.getLogger(__name__)
 
-class CallbackRegistry():
-    """ Calss for managing callback registration and notifications """
+
+class CallbackRegistry:
+    """Manage callback registration and notifications."""
+
     WILDCARD_KEY = "WILDCARD"
 
     def __init__(self) -> None:
         self._callbacks = {}
 
-
-    def register_callback(self,
-        callback:Callable[[Appliance, str, any], None] | Callable[[Appliance, str], None] | Callable[[Appliance], None] | Callable[[], None],
-        keys:str|Events|Sequence[str|Events],
-        appliance:Appliance|str = None
+    def register_callback(
+        self,
+        callback: Callable[[Appliance, str, Any], None]
+        | Callable[[Appliance, str], None]
+        | Callable[[Appliance], None]
+        | Callable[[], None],
+        keys: str | Events | Sequence[str | Events],
+        appliance: Appliance | str = None,
     ):
-        """ Register callback for change event notifications
+        """Register callback for change event notifications.
 
-        Use the Appliance.register_callback() to register for appliance data update events
+        Use the Appliance.register_callback() to register for appliance data update events.
 
         Parameters:
         * callback - A callback function to call when the event occurs, all the parameters are optional
@@ -35,7 +42,7 @@ class CallbackRegistry():
         """
 
         if not isinstance(keys, list):
-            keys = [ keys ]
+            keys = [keys]
 
         haid = appliance.haId if isinstance(appliance, Appliance) else appliance
 
@@ -43,30 +50,36 @@ class CallbackRegistry():
             self._callbacks[haid] = {}
 
         for key in keys:
-            if '*' in key:
+            if "*" in key:
                 callback_record = {
                     "key": key,
                     "regex": re.compile(fnmatch.translate(key), re.IGNORECASE),
-                    "callback": callback
+                    "callback": callback,
                 }
                 if self.WILDCARD_KEY not in self._callbacks[haid]:
                     self._callbacks[haid][self.WILDCARD_KEY] = []
-                if not self.wildcard_registered(callback_record, self._callbacks[haid][self.WILDCARD_KEY]):
+                if not self.wildcard_registered(
+                    callback_record, self._callbacks[haid][self.WILDCARD_KEY]
+                ):
                     self._callbacks[haid][self.WILDCARD_KEY].append(callback_record)
             else:
                 if key not in self._callbacks[haid]:
                     self._callbacks[haid][key] = set()
                 self._callbacks[haid][key].add(callback)
 
-    def deregister_callback(self,
-        callback:Callable[[Appliance, str, any], None] | Callable[[Appliance, str], None] | Callable[[Appliance], None] | Callable[[], None],
-        keys:str|Events|Sequence[str|Events],
-        appliance:Appliance|str = None
+    def deregister_callback(
+        self,
+        callback: Callable[[Appliance, str, any], None]
+        | Callable[[Appliance, str], None]
+        | Callable[[Appliance], None]
+        | Callable[[], None],
+        keys: str | Events | Sequence[str | Events],
+        appliance: Appliance | str = None,
     ):
-        """ Clear a callback that was prevesiously registered so it stops getting notifications """
+        """Clear a callback that was prevesiously registered so it stops getting notifications."""
 
         if not isinstance(keys, list):
-            keys = [ keys ]
+            keys = [keys]
 
         haid = appliance.haId if isinstance(appliance, Appliance) else appliance
 
@@ -74,41 +87,55 @@ class CallbackRegistry():
             self._callbacks[haid] = {}
 
         for key in keys:
-            if '*' in key:
-                if haid in self._callbacks and self.WILDCARD_KEY in self._callbacks[haid]:
-                    new_list = [ item for item in self._callbacks[haid][self.WILDCARD_KEY] if item['key'] != key or item['callback'] != callback]
+            if "*" in key:
+                if (
+                    haid in self._callbacks
+                    and self.WILDCARD_KEY in self._callbacks[haid]
+                ):
+                    new_list = [
+                        item
+                        for item in self._callbacks[haid][self.WILDCARD_KEY]
+                        if item["key"] != key or item["callback"] != callback
+                    ]
                     self._callbacks[haid][self.WILDCARD_KEY] = new_list
             else:
                 if haid in self._callbacks and key in self._callbacks[haid]:
                     self._callbacks[haid][key].remove(callback)
 
-    def wildcard_registered(self,  callback_record, callback_list) -> bool:
-        """ Checks if the key and callback pair are already in the list of callbacks """
+    def wildcard_registered(self, callback_record, callback_list) -> bool:
+        """Checks if the key and callback pair are already in the list of callbacks."""
         for item in callback_list:
-            if item['key'] == callback_record['key'] and item['callback'] == callback_record['callback']:
+            if (
+                item["key"] == callback_record["key"]
+                and item["callback"] == callback_record["callback"]
+            ):
                 return True
         return False
 
     def clear_all_callbacks(self):
-        """ Clear all the registered callbacks """
+        """Clear all the registered callbacks."""
         self._callbacks = {}
 
-    def clear_appliance_callbacks(self, appliance:Appliance|str):
-        """ Clear all the registered callbacks """
+    def clear_appliance_callbacks(self, appliance: Appliance | str):
+        """Clear all the registered callbacks."""
         haid = appliance.haId if isinstance(appliance, Appliance) else appliance
 
         if haid in self._callbacks:
             del self._callbacks[haid]
 
-    async def async_broadcast_event(self, appliance:Appliance, event_key:str|Events, value:any = None) -> None:
-        """ Broadcast an event to all subscribed callbacks """
+    async def async_broadcast_event(
+        self, appliance: Appliance, event_key: str | Events, value: any = None
+    ) -> None:
+        """Broadcast an event to all subscribed callbacks."""
 
         _LOGGER.debug("Broadcasting event: %s = %s", event_key, str(value))
-        handled:bool = False
+        handled: bool = False
         haid = appliance.haId
 
         # dispatch simple event callbacks
-        handlers = [ handler for handler in [None, appliance.haId] if handler in self._callbacks]
+        handlers = [
+            handler for handler in [None, appliance.haId] if handler in self._callbacks
+        ]
         for haid in handlers:
             if event_key in self._callbacks[haid]:
                 for callback in self._callbacks[haid][event_key]:
@@ -119,7 +146,7 @@ class CallbackRegistry():
             if self.WILDCARD_KEY in self._callbacks[haid]:
                 for callback_record in self._callbacks[haid][self.WILDCARD_KEY]:
                     if callback_record["regex"].fullmatch(event_key):
-                        callback = callback_record['callback']
+                        callback = callback_record["callback"]
                         await self._async_call(callback, appliance, event_key, value)
                         handled = True
 
@@ -128,9 +155,14 @@ class CallbackRegistry():
                 for callback in self._callbacks[Events.UNHANDLED]:
                     self._async_call(callback, appliance, event_key, value)
 
-
-    async def _async_call(self, callback:Callable, appliance:Appliance, event_key:str|Events, value:any) -> None:
-        """ Helper funtion to make the right kind of call to the callback funtion """
+    async def _async_call(
+        self,
+        callback: Callable,
+        appliance: Appliance,
+        event_key: str | Events,
+        value: any,
+    ) -> None:
+        """Helper funtion to make the right kind of call to the callback funtion."""
         sig = inspect.signature(callback)
         param_count = len(sig.parameters)
         callback_error = False
@@ -159,6 +191,10 @@ class CallbackRegistry():
                 else:
                     callback_error = True
         except Exception as ex:
-            _LOGGER.warning("Unhandled exception in callback function for event_key: %s", event_key, exc_info=ex)
+            _LOGGER.warning(
+                "Unhandled exception in callback function for event_key: %s",
+                event_key,
+                exc_info=ex,
+            )
         if callback_error:
             raise ValueError(f"Unexpected number of callback parameters: {sig}")
