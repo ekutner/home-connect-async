8313713823a5692b1b6bc0c130a281908001e0e6
ekutner <>
2023-02-25 17:45:41 +0200

    Replace cchardet dependency with charset_normalizer Restore the dataclasses-json dependency to 0.5.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'home-connect-async',
     packages = ['home_connect_async'],
-    version = '0.7.6',
+    version = '0.7.7',
     license='MIT',
     description = 'Async SDK for BSH Home Connect API',
     author = 'Eran Kutner',
@@ -13,18 +13,17 @@ setup(
     install_requires=[
         'aiohttp',
         'aiohttp-sse-client>=0.2.1',
-        'dataclasses-json>=0.5.3',
+        'dataclasses-json>=0.5.6',
         'oauth2-client>=1.2.1',
-        'cchardet'
+        'charset_normalizer'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
